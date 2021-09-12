cba90c2ed9c616e767e8d5cf07be6ea8d141e5f1
Krvoje <hperadin@gmail.com>
2021-09-12 13:06:23 +0200

    Add script for polyrhythm generation

---
+++ README.md
@@ -0,0 +1,30 @@
+A small script to print out polyrhythms for a given combination of beats.
+
+Prints out the individual beats, and then their combination.
+
+Examples:
+
+Print out a 3:2 polyrhythm:
+```
+./polyrhythm.py 3 2
+x.x.x.
+x..x..
+x.xxx.
+```
+
+Print out a 4:3 polyrhythm:
+```
+./polyrhythm.py 4 3
+x..x..x..x..
+x...x...x...
+x..xx.x.xx..
+```
+
+Print out a 4:5:6 polyrhythm:
+```
+./polyrhythm.py 4 5 6
+x...x...x...x...x...x...x...x...x...x...x...x...x...x...x...x...x...x...x...x...x...x...x...x...x...x...x...x...x...x...
+x....x....x....x....x....x....x....x....x....x....x....x....x....x....x....x....x....x....x....x....x....x....x....x....
+x.....x.....x.....x.....x.....x.....x.....x.....x.....x.....x.....x.....x.....x.....x.....x.....x.....x.....x.....x.....
+x...xxx.x.x.x..xx.x.x...xx..x.x.x..xx...x.x.xx..x.x.x.xxx...x...xxx.x.x.x..xx.x.x...xx..x.x.x..xx...x.x.xx..x.x.x.xxx...
+```
---
+++ polyrhythm.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+import sys
+
+X='x'
+
+def mult(l):
+    res = 1
+    for x in l: res = res*x
+    return res
+
+def leastCommonMultiple(ms):
+    ds = set(ms)
+    ms = set(ms)
+    for x in ds:
+        for y in ds:
+            if x > y and x % y == 0 and x in ms:
+                ms.remove(x)
+                ms.add(int(x/y))
+    return mult(set(ms))
+
+def mono(r,lcm):
+    lm = lambda x: X if x%r == 0 else '.'
+    m = "".join(list(map(lm, range(lcm))))
+    print(m)
+
+def divides(ms, x):
+    for m in ms:
+        if x%m == 0: return True
+    return False
+
+def poly(ms):
+    lcm = leastCommonMultiple(ms)
+    for m in ms:
+        mono(m, lcm)
+    lz = lambda x: X if divides(ms, x) else '.'
+    z = "".join(list(map(lz, range(lcm))))
+    print(z)
+
+if len(sys.argv) < 2:
+    print("""
+Prints out polyrhythms for a given combination of beats.
+First rows are the individual beats, the last row is the combined rhythym.
+
+Examples:
+
+# Prints a 3:2 polyrhythm
+{0} 3 2
+x.x.x.
+x..x..
+x.xxx.
+
+# Prints a 3:4:5 polyrhythm
+{0} 3 4 5
+x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..
+x...x...x...x...x...x...x...x...x...x...x...x...x...x...x...
+x....x....x....x....x....x....x....x....x....x....x....x....
+x..xxxx.xxx.x..xx.x.xx..xx.xx.x.xx.xx..xx.x.xx..x.xxx.xxxx..
+""".format(sys.argv[0]))
+    print("For example: ./poliritmovi.py 3 2 for 3:2 polyrhythm")
+else:
+    measures = sorted([int(x) for x in sys.argv[1:]])
+    poly(measures)
